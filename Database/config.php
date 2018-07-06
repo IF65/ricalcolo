91ed35d01778e27e8ee1296b376f1fa8be06f38c
Marco Gnecchi <marco.gnecchi@icloud.com>
2018-07-06 14:32:11 +0200

    allineamento

--- Database/config.php
+++ Database/config.php
@@ -1,6 +1,6 @@
 <?php
 
-// (DEBUG) Attiva la visualizzazione degli errori in fase di debug (rimuovere in produzione)
+// (DEBUG 1) Attiva la visualizzazione degli errori in fase di debug (rimuovere in produzione)
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
