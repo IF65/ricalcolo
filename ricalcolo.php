eed775a456befce0404d3a726b737aeee077ee90
Marco Gnecchi <marco.gnecchi@icloud.com>
2020-01-13 15:58:11 +0100

    allineamento

--- ricalcolo.php
+++ ricalcolo.php
@@ -59,15 +59,15 @@
 	//--------------------------------------------------------------------------------
     $timeZone = new DateTimeZone('Europe/Rome');
 
-	/*$end = (new DateTime())->sub(new DateInterval('P1D'));
+	$end = (new DateTime())->sub(new DateInterval('P1D'));
 	$start = new DateTime('January 1, '.$end->format('Y'));
 	$interval = new DateInterval('P1D');
-	$range = new DatePeriod($start, $interval, $end);*/
+	$range = new DatePeriod($start, $interval, $end);
 
-    $end = DateTime::createFromFormat('Y-m-d', '2019-12-31');
+/*    $end = DateTime::createFromFormat('Y-m-d', '2019-12-31');
     $start = DateTime::createFromFormat('Y-m-d', '2019-01-01');
     $interval = new DateInterval('P1D');
-    $range = new DatePeriod($start, $interval, $end);
+    $range = new DatePeriod($start, $interval, $end);*/
 
 	// serpentone
 	//--------------------------------------------------------------------------------
